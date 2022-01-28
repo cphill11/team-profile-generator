83691b7ec6f1bf9680bad9bf18ca9c44e6414d36
Criste Phillips <criste.phillips@yahoo.com>
2022-01-28 14:35:37 -0500

    initial testing started

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,27 +1,54 @@
-// this populated automatically when I was attempting to code for a test
-const { expect } = require('@jest/globals');
-
 // import & mock necessary modules to create a failing test that will check for proper object creation
 const Employee = require('../lib/Employee.js');
 
-jest.mock('../lib/Employee.js');
-
-test('creates a new employee card', () => {
-    const employee = new Employee('');
-
-    expect(employee.name).toBe();
-    expect(employee.id).toBe();
-    expect(employee.email).toBe();
-
+test('constructor name argument', () => {
+    // set test variable
+    const name = 'Bob'
+    const employee = new Employee(name)
+        expect(employee.name).toBe(name)
 })
 
-// Employee class is first parent class
 
-// Properties & methods include:
-// name
-// id
-// email
-// getName()
-// getID()
-// getEmail()
-// getRole()   // returns 'Employee'
+test('constructor id argument', () => {
+    // set test variable
+    const id = 1234
+    const employee = new Employee("Bob", id)
+        expect(employee.id).toBe(id)
+})
+
+test('constructor email argument', () => {
+    // set test variable
+    const email = 'bob@gmail'
+    const employee = new Employee("Bob", 1234, email)
+        expect(employee.email).toBe(email)
+})
+
+// start methods; use () in GetName b/c it's a method.
+test('get name method', () => {
+    // set test variable
+    const name = 'Bob'
+    const employee = new Employee(name)
+        expect(employee.getName()).toBe(name)
+})
+
+
+test('get id method', () => {
+    // set test variable
+    const id = 1234
+    const employee = new Employee("Bob", id)
+        expect(employee.getID()).toBe(id)
+})
+
+test('get email method', () => {
+    // set test variable
+    const email = 'bob@gmail'
+    const employee = new Employee("Bob", 1234, email)
+        expect(employee.getEmail()).toBe(email)
+})
+
+test('get role  method', () => {
+    // set test variable
+    const role = 'Employee'
+    const employee = new Employee("Bob", 1234, 'email')
+        expect(employee.getRole()).toBe(role)
+})
