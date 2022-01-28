13355e106fdbc155345f715ab2a7fef1e845a150
Criste Phillips <criste.phillips@yahoo.com>
2022-01-28 14:57:08 -0500

    testing added to manager

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,28 @@
 // import & mock necessary modules to create a failing test that will check for proper object creation
-const Employee = require('../lib/Employee.js');
 const Engineer = require('../lib/Engineer.js');
 
-jest.mock('../lib/Engineer.js');
+
+test('constructor github argument', () => {
+    // set test variable
+    const github = 'Chuck'
+    const engineer = new Engineer("name", 1234, "email", github)
+        expect(engineer.github).toBe(github)
+})
+
+test('get github method', () => {
+    // set test variable
+    const github = 'Chuck'
+    const engineer = new Engineer("name", 1234, "email", github)
+        expect(engineer.getGithub()).toBe(github)
+})
+
+test('getRole method', () => {
+    // set test variable
+    const getrole = 'Engineer'
+    const engineer = new Engineer("name", 1234, "email", 
+    "github")
+        expect(engineer.getRole()).toBe(getrole)
+})
 
 // Other 3 classes will extend Employee class + more properties/ methods
 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,27 @@
 // import & mock necessary modules to create a failing test that will check for proper object creation
-const Employee = require('../lib/Employee.js');
 const Manager = require('../lib/Manager.js');
 
-jest.mock('../lib/Manager.js');
+test('constructor officeNumber argument', () => {
+    // set test variable
+    const officenumber= 567
+    const manager = new Manager("name", 1234, "email", officenumber)
+        expect(manager.officeNumber).toBe(officenumber)
+})
+
+test('get officeNumber method', () => {
+    // set test variable
+    const officenumber = 567
+    const manager = new Manager("name", 1234, "email", officenumber)
+        expect(manager.getOfficeNumber()).toBe(officenumber)
+})
+
+test('getRole method', () => {
+    // set test variable
+    const getrole = 'Manager'
+    const manager = new Manager("name", 1234, "email", 
+    567)
+        expect(manager.getRole()).toBe(getrole)
+})
 
 // Other 3 classes will extend Employee class + more properties/ methods
 // Manager class
